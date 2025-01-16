e84cc8c191f4deee6eb1409c60943cece31bd820
oplaalvarez0001 <oplaalvarez@microsoft.com>
2025-01-16 11:41:23 -0500

    Basket Microservice Completed

---
+++ src/Services/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName):IQuery<GetBasketResult>;
+
+    public record GetBasketResult(ShoppingCart Cart);
+    public class StoreBasketQueryHandler(IBasketRepository _repository) : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO: get basket from db
+            var basket = await _repository.GetBasket(query.UserName);
+
+            return new GetBasketResult(basket);
+
+        }
+    }
+}
---
+++ src/Services/Basket.API/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using BuildingBlocks.Behaviors;
+global using HealthChecks.UI.Client;
+global using Marten;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Basket.API.Exceptions;
+global using Basket.API.Data;
+global using FluentValidation;
